f2a4f93a6adc828f5f7c7ae5dda5ee4c32f852aa
Tommy Nguyen <tommytimmytony@gmail.com>
2024-11-20 23:53:00 -0600

    Update Backbutton.jsx

--- client/src/components/BackButton.jsx
+++ client/src/components/BackButton.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { useUser } from "../context/UserContext";
 import { PiArrowFatLeftFill } from "react-icons/pi";
 import { GiPopcorn } from "react-icons/gi";
+import { FaHome } from "react-icons/fa";
 
 export default function BackButton() {
   const navigate = useNavigate();
@@ -17,6 +18,10 @@ export default function BackButton() {
   const goback = () => {
     navigate(-1);
   };
+
+  const gohome= () => {
+    navigate("/movie-page");
+  }
   return (
     <>
       {location.pathname == "/movie-page" ? (
@@ -24,13 +29,22 @@ export default function BackButton() {
           MovieTrackr <GiPopcorn />
         </h2>
       ) : (
-        <button
-          onClick={goback}
-          className="flex items-center bg-electricBlue text-darkCharcoal py-2 px-4 rounded-lg font-bold hover:bg-cyberYellow hover:scale-105 transition duration-300"
-        >
-          <PiArrowFatLeftFill />
-          Back
-        </button>
+        <div className="flex self-center space-x-4">
+          {/* <button
+            onClick={goback}
+            className="flex items-center bg-electricBlue text-darkCharcoal py-2 px-4 rounded-lg font-bold hover:bg-cyberYellow hover:scale-105 transition duration-300"
+          >
+            <PiArrowFatLeftFill />
+            Back
+          </button> */}
+          <button
+            onClick={gohome}
+            className="flex items-center bg-electricBlue text-darkCharcoal py-2 px-4 rounded-lg font-bold hover:bg-cyberYellow hover:scale-105 transition duration-300"
+          >
+            <FaHome />
+            Home
+          </button>
+        </div>
       )}
     </>
   );
